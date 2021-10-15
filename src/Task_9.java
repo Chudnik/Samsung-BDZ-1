c41ecc9ca75c71becb25d0c3cb8c7eae0b690e0e
Chudnik <chud.nikita548@yandex.ru>
2021-10-15 17:17:43 +0300

    Commit

--- src/Task_9.java
+++ src/Task_9.java
@@ -5,13 +5,20 @@ public class Task_9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.ENGLISH);
-        String n = in.nextLine();
-        for (int i = 0; i < n.length(); i++) {
-            if (n.charAt(i) != n.charAt(n.length() - i)) {
-                System.out.print("No");
-                break;
-            }
-        }System.out.print("Yes");
+        long n = in.nextLong();
+        long a = n;
+        long last = 0;
+        long x = 0;
+        while (n > 0) {
+            last = n % 10;
+            x = (x * 10) + last;
+            n = n / 10;
+        }
+        if (x == a) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
 
